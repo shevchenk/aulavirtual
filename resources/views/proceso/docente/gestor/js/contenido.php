683506c88161f3f255aac5fe11295a8f8816dd02
Renzo CB <rcapchab@gmail.com>
2017-11-08 11:18:44 -0500

    Estructura modificada

---
+++ resources/views/proceso/docente/gestor/js/contenido.php
@@ -0,0 +1,221 @@
+<script type="text/javascript">
+var AddEdit=0; //0: Editar | 1: Agregar
+var ContenidoG={id:0,curso_id:0,contenido:'',ruta_contenido:'',file_archivo:'',tipo_respuesta:0,fecha_inicio:'',
+fecha_final:'',fecha_ampliada:'',estado:1}; // Datos Globales
+$(document).ready(function() {
+     $("#TableContenido").DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false
+    });
+    
+    $(".fecha").datetimepicker({
+        format: "yyyy-mm-dd",
+        language: 'es',
+        showMeridian: true,
+        time:true,
+        minView:2,
+        autoclose: true,
+        todayBtn: false
+    }); 
+
+    $('#ModalContenido').on('shown.bs.modal', function (event) {
+        if( AddEdit==1 ){
+            $(this).find('.modal-footer .btn-primary').text('Guardar').attr('onClick','AgregarEditarAjax3();');
+        }
+        else{
+            
+            $(this).find('.modal-footer .btn-primary').text('Actualizar').attr('onClick','AgregarEditarAjax3();');
+            $("#ModalContenidoForm").append("<input type='hidden' value='"+ContenidoG.id+"' name='id'>");
+        }
+
+        $('#ModalContenidoForm #txt_contenido').val( ContenidoG.contenido );
+        $('#ModalContenidoForm #txt_file_nombre').val( ContenidoG.ruta_contenido );
+        $('#ModalContenidoForm #txt_file_archivo').val( ContenidoG.file_archivo );
+        $('#ModalContenidoForm #slct_tipo_respuesta').selectpicker('val', ContenidoG.tipo_respuesta );
+        $('#ModalContenidoForm #txt_fecha_inicio').val( ContenidoG.fecha_inicio );
+        $('#ModalContenidoForm #txt_fecha_final').val( ContenidoG.fecha_final );
+        $('#ModalContenidoForm #txt_fecha_ampliada').val( ContenidoG.fecha_ampliada );
+        $('#ModalContenidoForm #slct_estado').selectpicker( 'val',ContenidoG.estado );
+        //$('#ModalContenidoForm #txt_razon_social').focus();
+    });
+
+    $('#ModalContenido').on('hidden.bs.modal', function (event) {
+        $("#ModalContenidoForm input[type='hidden']").not('.mant').remove();
+    });
+    
+    $( "#ModalContenidoForm #slct_tipo_respuesta" ).change(function() {
+        if( $('#ModalContenidoForm #slct_tipo_respuesta').val()=='1' ) {
+            $( "#ModalContenidoForm #respuesta" ).css("display","");
+        }else{
+            $( "#ModalContenidoForm #respuesta" ).css("display","none");
+        }
+
+    });
+});
+
+ValidaForm3=function(){
+    var r=true;
+
+    if( $.trim( $("#ModalContenidoForm #txt_contenido").val() )=='' ){
+        r=false;
+        msjG.mensaje('warning','Ingrese Contenido',4000);
+    }
+     else if( $.trim( $("#ModalContenidoForm #txt_file_nombre").val() )=='' ){
+        r=false;
+        msjG.mensaje('warning','Ingrese Ruta de Contenido',4000);
+    }
+    else if( $.trim( $("#ModalContenidoForm #slct_tipo_respuesta").val() )=='' ){
+        r=false;
+        msjG.mensaje('warning','Seleccione Tipo de Respuesta',4000);
+    }
+    else if( $.trim( $("#ModalContenidoForm #txt_fecha_inicio").val() )=='' && $.trim( $("#ModalContenidoForm #slct_tipo_respuesta").val() )=='1'){
+        r=false;
+        msjG.mensaje('warning','Ingrese Fecha Inicio',4000);
+    }
+    else if( $.trim( $("#ModalContenidoForm #txt_fecha_final").val() )=='' && $.trim( $("#ModalContenidoForm #slct_tipo_respuesta").val() )=='1' ){
+        r=false;
+        msjG.mensaje('warning','Ingrese Fecha Final',4000);
+    }
+    else if( $.trim( $("#ModalContenidoForm #txt_fecha_ampliada").val() )=='' && $.trim( $("#ModalContenidoForm #slct_tipo_respuesta").val() )=='1'){
+        r=false;
+        msjG.mensaje('warning','Ingrese Fecha Ampliada',4000);
+    }
+    return r;
+}
+
+AgregarEditar3=function(val,id){
+    AddEdit=val;
+    ContenidoG.id='';
+    ContenidoG.contenido='';
+    ContenidoG.ruta_contenido='';
+    ContenidoG.file_archivo='';
+    ContenidoG.tipo_respuesta='';
+    ContenidoG.fecha_inicio='';
+    ContenidoG.fecha_final='';
+    ContenidoG.fecha_ampliada='';
+    ContenidoG.estado='1';
+    $('#respuesta').css("display","none");
+    if( val==0 ){
+
+        ContenidoG.id=id;
+        ContenidoG.contenido=$("#TableContenido #trid_"+id+" .contenido").text();
+        ContenidoG.ruta_contenido=$("#TableContenido #trid_"+id+" .ruta_contenido").text();
+        ContenidoG.tipo_respuesta=$("#TableContenido #trid_"+id+" .tipo_respuesta").val();
+        ContenidoG.fecha_inicio=$("#TableContenido #trid_"+id+" .fecha_inicio").text();
+        ContenidoG.fecha_final=$("#TableContenido #trid_"+id+" .fecha_final").text();
+        ContenidoG.fecha_ampliada=$("#TableContenido #trid_"+id+" .fecha_ampliada").text();
+        ContenidoG.estado=$("#TableContenido #trid_"+id+" .estado").val();
+        if(ContenidoG.tipo_respuesta=='1'){
+                $('#respuesta').css("display","");
+        }
+    }
+    $('#ModalContenido').modal('show');
+}
+
+CambiarEstado3=function(estado,id){
+    sweetalertG.confirm("¿Estás seguro?", "Confirme la eliminación", function(){
+        AjaxContenido.CambiarEstado(HTMLCambiarEstado3,estado,id);
+    });
+}
+
+HTMLCambiarEstado3=function(result){
+    if( result.rst==1 ){
+        msjG.mensaje('success',result.msj,4000);
+        AjaxContenido.Cargar(HTMLCargarContenido);
+    }
+}
+
+AgregarEditarAjax3=function(){
+    if( ValidaForm3() ){
+        AjaxContenido.AgregarEditar(HTMLAgregarEditar3);
+    }
+}
+
+HTMLAgregarEditar3=function(result){
+    if( result.rst==1 ){
+        msjG.mensaje('success',result.msj,4000);
+        $('#ModalContenido').modal('hide');
+        AjaxContenido.Cargar(HTMLCargarContenido);
+    }
+    else{
+        msjG.mensaje('warning',result.msj,3000);
+    }
+}
+
+HTMLCargarContenido=function(result){
+    var html="";
+    $('#TableContenido').DataTable().destroy();
+
+    $.each(result.data,function(index,r){
+        estadohtml='<a id="'+r.id+'" onClick="CambiarEstado3(1,'+r.id+')" class="btn btn-danger btn-sm"><i class="fa fa-trash fa-lg"></i></a>';
+        if(r.estado==1){
+            estadohtml='<a id="'+r.id+'" onClick="CambiarEstado3(0,'+r.id+')" class="btn btn-danger btn-sm"><i class="fa fa-trash fa-lg"></i></a>';
+        }
+
+        html+="<tr id='trid_"+r.id+"'>"+
+            "<td class='curso'>"+r.curso+"</td>"+
+            "<td class='contenido'><a href='file/content/"+r.ruta_contenido+"'>"+r.contenido+"</a></td>"+
+            "<td class='tipo_respuesta_nombre'>"+r.tipo_respuesta_nombre+"</td>"+
+            "<td class='fecha_inicio'>"+r.fecha_inicio+"</td>"+
+            "<td class='fecha_final'>"+r.fecha_final+"</td>"+
+            "<td class='fecha_ampliada'>"+r.fecha_ampliada+"</td>"+
+            "<input type='hidden' class='tipo_respuesta' value='"+r.tipo_respuesta+"'>"+
+            "<input type='hidden' class='curso_id' value='"+r.curso_id+"'>"+
+            "<td>";
+            html+="<input type='hidden' class='estado' value='"+r.estado+"'>"+estadohtml+"</td>"+
+            '<td><a class="btn btn-primary btn-sm" onClick="AgregarEditar3(0,'+r.id+')"><i class="fa fa-edit fa-lg"></i> </a></td>'+
+            '<td><a class="btn btn-info btn-sm" onClick="CargarContenidoProgramacion('+r.id+','+r.programacion_unica_id+')"><i class="fa fa-th-list fa-lg"></i> </a></td>';
+        html+="</tr>";
+    });
+    $("#TableContenido tbody").html(html); 
+    $("#TableContenido").DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false
+        
+    });
+};
+CargarSlct=function(slct){
+    if(slct==1){
+    AjaxContenido.CargarCurso(SlctCargarCurso);
+    }
+};
+SlctCargarCurso=function(result){
+    var html="<option value='0'>.::Seleccione::.</option>";
+    $.each(result.data,function(index,r){
+        html+="<option value="+r.id+">"+r.curso+"</option>";
+    });
+    $("#ModalContenidoForm #slct_curso_id").html(html);
+    $("#ModalContenidoForm #slct_curso_id").selectpicker('refresh');
+
+};
+CargarContenidoProgramacion=function(id,programacion_unica_id){
+     $("#ContenidoProgramacionForm #txt_contenido_id").val(id);
+     $("#ModalContenidoProgramacionForm #txt_contenido_id").val(id);
+     $("#ModalContenidoProgramacionForm #btn_listarpersona").data( 'filtros', 'estado:1|programacion_unica_id:'+programacion_unica_id );
+     AjaxContenidoProgramacion.Cargar(HTMLCargarContenidoProgramacion);
+     $("#ContenidoProgramacionForm").css("display","");
+     
+};
+onImagen = function (event) {
+        var files = event.target.files || event.dataTransfer.files;
+        if (!files.length)
+            return;
+        var image = new Image();
+        var reader = new FileReader();
+        reader.onload = (e) => {
+            $('#ModalContenidoForm #txt_file_archivo').val(e.target.result);
+            $('#ModalContenidoForm .img-circle').attr('src',e.target.result);
+        };
+        reader.readAsDataURL(files[0]);
+        $('#ModalContenidoForm #txt_file_nombre').val(files[0].name);
+        console.log(files[0].name);
+};
+</script>
