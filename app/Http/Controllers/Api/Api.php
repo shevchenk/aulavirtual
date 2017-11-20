b588020e3bd6b860293512ba60da13ab65270c2d
Renzo CB <rcapchab@gmail.com>
2017-11-20 11:15:34 -0500

    a

--- app/Http/Controllers/Api/Api.php
+++ app/Http/Controllers/Api/Api.php
@@ -32,14 +32,14 @@ class Api extends Controller
         }
     }
 
-    public function curl($url, $data)
+    public function curl($url, $data=array())
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, (array)$data);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
         $result = curl_exec($ch);
         curl_close($ch);
         return json_decode($result);
