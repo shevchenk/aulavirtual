609a30a102cb5d5c497aa948e439361205036876
Renzo CB <rcapchab@gmail.com>
2017-12-04 14:41:25 -0500

    a

--- resources/views/proceso/master/gestor/js/copiacontenido_ajax.php
+++ resources/views/proceso/master/gestor/js/copiacontenido_ajax.php
@@ -7,7 +7,7 @@ var AjaxCopiaContenido={
     },
     Cargar:function(evento){
         var curso_id=$("#ModalCopiaContenidoForm #txt_curso_id").val();
-        var data={curso_id:curso_id};
+        var data={curso_id:curso_id,tipo_respuesta:0};
         url='AjaxDinamic/Proceso.ContenidoPR@Load';
         $("#ModalCopiaContenidoForm input[type='hidden']").not('.mant').remove();
         masterG.postAjax(url,data,evento);
