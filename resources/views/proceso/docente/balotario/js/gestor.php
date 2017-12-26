b38e75558724935a69d15e0fa063942c36274004
Renzo CB <rcapchab@gmail.com>
2017-12-26 14:40:11 -0500

    Docente

--- resources/views/proceso/docente/balotario/js/gestor.php
+++ resources/views/proceso/docente/balotario/js/gestor.php
@@ -38,6 +38,8 @@ HTMLCargarProgramacionUnica=function(result){
     $.each(result.data.data,function(index,r){
 
         html+='<tr id="trid_'+r.id+'" onClick="CargarBalotario('+r.id+','+r.curso_id+',\''+r.curso+'\',this)">'+
+            "<td class='carrera'>"+r.carrera+"</td>"+
+            "<td class='ciclo'>"+r.ciclo+"</td>"+
             "<td class='curso'>"+
             "<a target='_blank' href='img/course/"+r.foto+"'>"+
             "<img src='img/course/"+r.foto+"' style='height: 40px;width: 40px;'>"+
--- resources/views/proceso/docente/balotario/js/gestor_ajax.php
+++ resources/views/proceso/docente/balotario/js/gestor_ajax.php
@@ -7,10 +7,7 @@ var AjaxProgramacionUnica={
         data=$("#ProgramacionUnicaForm").serialize().split("txt_").join("").split("slct_").join("");
         $("#ProgramacionUnicaForm input[type='hidden']").not('.mant').remove();
         
-        if($('#txt_dni').val() != '')
-          url='AjaxDinamic/Proceso.ProgramacionUnicaPR@Load';
-        else
-          url='AjaxDinamic/Proceso.ProgramacionUnicaPR@validarProgramacion';
+        url='AjaxDinamic/Proceso.ProgramacionUnicaPR@validarProgramacion';
         masterG.postAjax(url,data,evento);
     }
 };
