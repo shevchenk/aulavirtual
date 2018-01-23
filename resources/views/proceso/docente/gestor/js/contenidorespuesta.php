ade7cda235201925dfdd83ac30c336628573e013
cyberusbel <rusbelc02@gmail.com>
2018-01-22 23:36:49 -0500

    c

--- resources/views/proceso/docente/gestor/js/contenidorespuesta.php
+++ resources/views/proceso/docente/gestor/js/contenidorespuesta.php
@@ -18,18 +18,19 @@ HTMLCargarContenidoRespuesta=function(result){
     $('#TableContenidoRespuesta').DataTable().destroy();    
 
     $.each(result.data,function(index,r) {
-        if(r.nota != null)
-            var nota = r.nota;
-        else
-            var nota = '';
-
         html+="<tr id='trid_"+r.id+"'>"+
             "<td class='alumno'>"+r.alumno+"</td>"+
             "<td class='respuesta'>"+r.respuesta+"</td>"+
             "<td class='ruta_respuesta'><a href='file/content/"+r.ruta_respuesta+"' target='blank'>"+r.ruta_respuesta+"</a></td>"+
-            "<td class='created_at'>"+r.created_at+"</td>"+
-            '<td class="nota"><textarea class="form-control" id="nota'+r.id+'" name="nota'+r.id+'" rows="4" style="width: 100%;">'+nota+'</textarea></td>'+
-            '<td class="nota"><button type="button" onClick="guardarNotaRpta('+r.id+', '+r.contenido_id+');" class="btn btn-primary" style="margin-top: 20%;">Guardar</button></td>';
+            "<td class='created_at'>"+r.created_at+"</td>";
+        if (r.nota != null) {
+            html+='<td class="nota">'+r.nota+'</td>';
+            html+='<td class="nota"><button type="button" onClick="" class="btn btn-default" disabled>Guardar</button></td>';          
+        } else {
+            html+='<td class="nota"><input type="text" class="form-control" id="nota'+r.id+'" name="nota'+r.id+'" style="width: 20%;" value="'+r.nota+'"></td>';
+            html+='<td class="nota"><button type="button" onClick="guardarNotaRpta('+r.id+', '+r.contenido_id+');" class="btn btn-primary">Guardar</button></td>';
+        }
+         
         html+="</tr>";
     });
     $("#TableContenidoRespuesta tbody").html(html); 
@@ -45,12 +46,28 @@ HTMLCargarContenidoRespuesta=function(result){
 };
 
 
+
 guardarNotaRpta=function(id, contenido_id){
-    //alert($('#nota'+id).val());
+
     $("#ContenidoRespuestaForm #txt_contenido_id").val(contenido_id);
     $("#ContenidoRespuestaForm #txt_contenido_respuesta_id").val(id);
     $("#ContenidoRespuestaForm #txt_nota_cr").val($('#nota'+id).val());
-    AjaxContenidoRespuesta.GuardarNotaRpta(HTMLGuardarNotaRpta);
+
+    swal({
+      title: "¿Confirmación?",   
+      text: "Desea guardar la nota, no podrá reversar los cambios...!",   
+      //type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#8CD4F5",
+      confirmButtonText: "Continuar!",
+      closeOnConfirm: true
+    },
+    function(){
+        //alert($('#nota'+id).val());
+        //$('#nota'+id).prop('readonly', true);
+        AjaxContenidoRespuesta.GuardarNotaRpta(HTMLGuardarNotaRpta);
+    });
+    
 };
 
 /*CambiarEstado=function(estado,id){
